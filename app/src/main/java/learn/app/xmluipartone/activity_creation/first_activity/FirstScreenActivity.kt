9114f79c61a1c71ef7b39db209d233f1dc223fa7
sharmajidev-labs <rs7992361@gmail.com>
2026-07-01 18:39:06 +0530

    basic knowladge or syntax

---
+++ app/src/main/java/learn/app/xmluipartone/activity_creation/first_activity/FirstScreenActivity.kt
@@ -0,0 +1,12 @@
+package learn.app.xmluipartone.activity_creation.first_activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import learn.app.xmluipartone.R
+
+class FirstScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_screen)
+
+    }
+}
