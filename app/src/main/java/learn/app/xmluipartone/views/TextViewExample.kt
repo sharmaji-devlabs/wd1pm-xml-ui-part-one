b7e864ac31d68d016612a7a17bdeee963a483f53
sharmajidev-labs <rs7992361@gmail.com>
2026-07-02 15:41:50 +0530

    TextView examples

---
+++ app/src/main/java/learn/app/xmluipartone/views/TextViewExample.kt
@@ -0,0 +1,21 @@
+package learn.app.xmluipartone.views
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import learn.app.xmluipartone.R
+
+class TextViewExample : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_text_view_example)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
