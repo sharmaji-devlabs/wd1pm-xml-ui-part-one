8825d99298db4b79ad37c539d953c54bdaa25698
sharmajidev-labs <rs7992361@gmail.com>
2026-07-09 18:42:13 +0530

    Button | and Event Handling

--- app/src/main/java/learn/app/xmluipartone/views/button_view_example/ButtonViewExample.kt
+++ app/src/main/java/learn/app/xmluipartone/views/button_view_example/ButtonViewExample.kt
@@ -1,6 +1,8 @@
 package learn.app.xmluipartone.views.button_view_example
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat
 import learn.app.xmluipartone.R
 
 class ButtonViewExample : AppCompatActivity() {
+    private lateinit var btnSubmit: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +20,29 @@ class ButtonViewExample : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // to find any view or view group activity provide findViewById() method (note : this is a generic method)
+        btnSubmit = findViewById(R.id.btn_submit);
+
+//        btnSubmit.text = "Only Submit"
+
+        btnSubmit.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                btnSubmit.text = "Clicked"
+            }
+        });
+
+        btnSubmit.setOnClickListener(Click())
+
     }
+//
+//    override fun onClick(v: View?) {
+//        btnSubmit.text = "Clicked"
+//    }
 }
+
+//class Click : View.OnClickListener{
+//    override fun onClick(v: View?) {
+//
+//    }
+//}
